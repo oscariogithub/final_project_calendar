d3d7ddd394cea5c8e9fe39e3feab7069f8ff2601
oscar <oscar.r.arancibia@gmail.com>
2019-07-17 12:13:17 -0400

    commit de respaldo

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -22,6 +22,7 @@ $(document).on('ready turbolinks:load', function(){
         defaultRangeSeparator:'00-30',
         
         
+        
         eventDrop: function(event, delta, revertFunc) {
                     console.log(delta);
                     $.ajax({
